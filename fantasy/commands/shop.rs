ee878621c75be26fffa4e63afdbd98608d655aaf
pancakerprotocol <74989553+pancakerprotocol@users.noreply.github.com>
2025-06-01 12:44:09 -0700

    Delete fantasy directory

--- fantasy/commands/shop.rs
+++
@@ -1,7 +0,0 @@
-pub async fn buy_item(user_id: i64, item_name: &str, pool: &PgPool) -> String {
-    // Fetch item price
-    // Check user balance
-    // Insert into user_items
-    // Update gold balance
-    format!("You bought a {}!", item_name)
-}
